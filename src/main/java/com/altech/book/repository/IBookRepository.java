b7be2221d5197e8fd3bb2db9394daefada60deeb
Aldy Manek <arnoldusmanek539@gmail.com>
2025-09-26 21:15:01 +0700

    create book repository

---
+++ src/main/java/com/altech/book/repository/IBookRepository.java
@@ -0,0 +1,1 @@
+package com.altech.book.repository;import com.altech.book.model.Book;import org.springframework.data.jpa.repository.JpaRepository;import org.springframework.stereotype.Repository;@Repositorypublic interface IBookRepository extends JpaRepository<Book, Long>{}
